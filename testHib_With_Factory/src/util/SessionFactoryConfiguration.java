293082cf375982563e83a6582d6df60aa459e213
Kasun Miuranga <kasunmiuranga2000@gmail.com>
2023-03-19 11:47:07 +0530

    Content Updated

--- testHib_With_Factory/src/util/SessionFactoryConfiguration.java
+++ testHib_With_Factory/src/util/SessionFactoryConfiguration.java
@@ -4,14 +4,16 @@ import entity.Customer;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-
+/**
+ * Singleton design pattern used
+ */
 public class SessionFactoryConfiguration {
     private static SessionFactoryConfiguration sessionFactoryConfiguration;
     private final Session session;
 
     private SessionFactoryConfiguration() {
         Configuration configuration = new Configuration();
-        configuration.configure();
+        configuration.configure("hibernate.cfg.xml");
         configuration.addAnnotatedClass(Customer.class);
 
         SessionFactory sessionFactory = configuration.buildSessionFactory();
